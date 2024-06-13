410d73b03067a3981c1bd21b9473d8d75c6b544b
ozangenc <ozantrb61ziganoylu@gmail.com>
2024-06-13 11:55:59 +0200

    FINAL

--- admin/src/php/utils/menu_public.php
+++ admin/src/php/utils/menu_public.php
@@ -1,10 +1,11 @@
 <div class="menu">
     <ul class="cards">
         <li class="card"><a href="index_.php?page=accueil.php">Accueil</a> </li>
-        <li class="card"><a href="index_.php?page=login.php">Connexion</a></li>
         <li class="card"><a href="index_.php?page=article.php">Articles</a></li>
         <li class="card"><a href="index_.php?page=creation.php">Creation</a></li>
         <li class="card"><a href="index_.php?page=nouveaute.php">Nouveautes</a></li>
+        <li class="card"><a href="index_.php?page=login.php">Connexion</a></li>
+
     </ul>
 </div>
 
